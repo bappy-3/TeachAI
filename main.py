b22b93206035196b893d7078c61986184341a11b
Al Mustafiz Bappy <90166777+bappy-3@users.noreply.github.com>
2025-03-31 23:14:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ def get_curriculum():
     return jsonify({"curriculum": curriculum})
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
+    #app.run(host="0.0.0.0", port=5000)
 
 
 
